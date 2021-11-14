6bcd103f7eefe3808b62105bdcb66c0839c57095
GeorgieYan <46608695+GeorgieYan@users.noreply.github.com>
2021-11-13 23:04:56 -0700

    Update hacked.py

--- hacked.py
+++ hacked.py
@@ -1,12 +1,15 @@
 import json
 
-def create_db():
-	study_spaces = {"Rutherford":{"Vacant": ["Room 1", "2", "3", "4", "5"], "Occupied": ["x", "y", "z"]}, "ECHA":{"Vacant": ["Room 1", "Room 2"], "Occupied": ["Room x", "Room y"]}} 
-	with open("study_space.json", "w") as outfile:
-		json.dump(study_spaces, outfile)
-	return outfile
 
-#class make_change(study_spaces):
+def create_db():
+    study_spaces = {"Rutherford": {"Vacant": ["Room 1", "2", "3", "4", "5"], "Occupied": ["x", "y", "z"]},
+                    "ECHA": {"Vacant": ["Room 1", "Room 2"], "Occupied": ["Room x", "Room y"]}}
+    with open("study_space.json", "w") as outfile:
+        json.dump(study_spaces, outfile)
+    return outfile
+
+
+# class make_change(study_spaces):
 #	return study_sp
 
 
@@ -14,14 +17,13 @@ outfile = create_db()
 
 # ----------------------------------------------
 with open('study_space.json', 'r') as openfile:
-  
     # Reading from json file
     json_object = json.load(openfile)
-  
+
 print(type(json_object))
 for vars in json_object:
-  print(vars)
-a="Rutherford"
+    print(vars)
+a = "Rutherford"
 print(json_object["Rutherford"]["Vacant"])
 json_object["Rutherford"]["Vacant"].remove("2")
 print(json_object["Rutherford"]["Vacant"])
@@ -30,21 +32,23 @@ print(type(json_object))
 # -----------------------------------------------
 
 with open('study_space.json', 'r') as openfile:
-  
     # Reading from json file
     json_object = json.load(openfile)
 for vars in json_object:
-  print(vars) 
-building = input("What is the building name?")
+    print(vars)
 
-print(json_object[building]["Vacant"])
-room = input("Which room would you like to study in?")
+options = input("sign-in or sign-out?")
+if options == "sign-in":
+    building = input("What is the building name?")
 
-if room in (json_object[building]["Vacant"]):
-  json_object[building]["Vacant"].remove(room)
-  json_object[building]["Occupied"].append(room)
-  print("You have successfully secured the "+room+" in the "+building+".")
-  with open("study_space.json", "w") as outfile:
-    json.dump(json_object, outfile)
-else:
-  print("Invalid output")
+    print(json_object[building]["Vacant"])
+    room = input("Which room would you like to study in?")
+
+    if room in (json_object[building]["Vacant"]):
+        json_object[building]["Vacant"].remove(room)
+        json_object[building]["Occupied"].append(room)
+        print("You have successfully secured the " + room + " in the " + building + ".")
+        with open("study_space.json", "w") as outfile:
+            json.dump(json_object, outfile)
+    else:
+        print("Invalid output")
