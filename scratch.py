3507867cb4281784776922ac5a865a7f9118dc64
Aidan <aidangironella@gmail.com>
2021-11-14 12:40:42 -0700

    clean up the repo

--- scratch.py
+++
@@ -1,6 +0,0 @@
-import json
-with open('credentials.json', 'r') as openfile:
-        userLoginDatabase = json.load(openfile)
-user = input('User: ')
-print(userLoginDatabase[user])
-# password = input('Password: ')
